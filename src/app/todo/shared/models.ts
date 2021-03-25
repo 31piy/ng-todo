1ac07df66386e4fa90c4de2863d5d00e9942a01e
Piyush Sonagara <piyush.sonagara@cohesity.com>
2021-03-25 19:12:23 +0530

    feat: Add models and data store service

---
+++ src/app/todo/shared/models.ts
@@ -0,0 +1,41 @@
+/**
+ * Represents a TODO list item.
+ */
+export interface TodoItem {
+  /**
+   * Unique ID of this item.
+   */
+  id: number;
+
+  /**
+   * A timestamp (in milliseconds from epoch) to indicate when this item was
+   * created.
+   */
+  createdAt: number;
+
+  /**
+   * A timestamp (in milliseconds from epoch) to indicate when this item was
+   * last updated.
+   */
+  lastUpdatedAt: number;
+
+  /**
+   * The title of this item. Defaults to an empty string.
+   */
+  title: string;
+
+  /**
+   * Indicates whether this item has been marked as done.
+   */
+  isDone: boolean;
+}
+
+/**
+ * A type to pass information for creating a TODO item.
+ */
+export type CreateTodoItemRequest = Pick<TodoItem, 'title'>;
+
+/**
+ * A type to pass information for updating a TODO item.
+ */
+export type UpdateTodoItemRequest = Pick<TodoItem, 'id' | 'title' | 'isDone'>;
---
+++ src/app/todo/shared/todo-datastore.service.ts
@@ -0,0 +1,101 @@
+import { Observable, of, throwError } from 'rxjs';
+import {
+  CreateTodoItemRequest,
+  TodoItem,
+  UpdateTodoItemRequest,
+} from './models';
+
+/**
+ * The prefix to use to store TODO items.
+ */
+const storeKeyPrefix = 'ng-todo';
+
+/**
+ * The key to store/retrieve items to/from the local storage.
+ */
+export const todoItemsKey = `${storeKeyPrefix}.items`;
+
+/**
+ * The data store for the TODO items. Uses local storage API to persist the
+ * items on the browser.
+ */
+export class TodoDataStoreService {
+  /**
+   * Returns the list of TODO items in the data store.
+   *
+   * @returns An observable of the TODO items.
+   */
+  getItems(): Observable<TodoItem[]> {
+    const items: TodoItem[] = JSON.parse(this.read());
+    return of(items);
+  }
+
+  /**
+   * Creates a TODO item.
+   *
+   * @param req The request parameters to create a new item.
+   * @returns An observable of the newly created TODO item.
+   */
+  createItem(req: CreateTodoItemRequest): Observable<TodoItem> {
+    const newItem: TodoItem = {
+      id: Date.now(),
+      createdAt: Date.now(),
+      lastUpdatedAt: Date.now(),
+      title: req.title,
+      isDone: false,
+    };
+
+    // Update the data store.
+    const storeItems: TodoItem[] = JSON.parse(this.read());
+    storeItems.push(newItem);
+    this.write(JSON.stringify(storeItems));
+
+    return of(newItem);
+  }
+
+  updateItem(req: UpdateTodoItemRequest): Observable<TodoItem> {
+    const storeItems: TodoItem[] = JSON.parse(this.read());
+    const existingItem = storeItems.find((item) => item.id === req.id);
+
+    if (!existingItem) {
+      return throwError(`No TODO item with ID ${req.id} was found.`);
+    }
+
+    // Mutate the item and update the store
+    existingItem.title = req.title;
+    existingItem.isDone = req.isDone;
+    existingItem.lastUpdatedAt = Date.now();
+    this.write(JSON.stringify(storeItems));
+
+    return of(existingItem);
+  }
+
+  deleteItem(itemId: TodoItem['id']): Observable<null> {
+    const storeItems: TodoItem[] = JSON.parse(this.read());
+    const itemIndex = storeItems.findIndex((item) => item.id === itemId);
+
+    if (itemIndex === -1) {
+      return throwError(`No TODO item with ID ${itemId} was found.`);
+    }
+
+    // Update the store
+    storeItems.splice(itemIndex, 1);
+    this.write(JSON.stringify(storeItems));
+
+    return of(null);
+  }
+
+  /**
+   * Returns the currently stored value of the data store.
+   */
+  private read(): string {
+    return localStorage.getItem(todoItemsKey) || JSON.stringify([]);
+  }
+
+  /**
+   * Writes a new value to the data store.
+   */
+  private write(newValue: string = JSON.stringify([])): void {
+    localStorage.setItem(todoItemsKey, newValue);
+  }
+}
