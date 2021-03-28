4e6a5375424cee41b51af65702c3cfb836f270d6
Piyush Sonagara <piyush.sonagara@cohesity.com>
2021-03-28 12:28:20 +0530

    test: Update unit tests

--- src/app/todo/list-item/list-item.component.spec.ts
+++ src/app/todo/list-item/list-item.component.spec.ts
@@ -1,3 +1,4 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { TodoItem } from '../shared';
 
@@ -19,6 +20,7 @@ describe('ListItemComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ListItemComponent],
+      schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents();
   });
 
@@ -33,13 +35,96 @@ describe('ListItemComponent', () => {
     expect(component).toBeTruthy();
   });
 
+  describe('onSelectionCheckboxChange', () => {
+    it('emits the toggleSelection event', () => {
+      spyOn(component.toggleSelection, 'next');
+      component.onSelectionCheckboxChange({ checked: true, source: null });
+      expect(component.toggleSelection.next).toHaveBeenCalledWith(true);
+    });
+  });
+
   describe('onItemTitleDblClick', () => {
+    beforeEach(() => {
+      spyOn(window, 'setTimeout');
+    });
+
     it('toggles the edit mode', () => {
       component.editMode = false;
       component.onItemTitleDblClick();
 
       expect(component.editMode).toBe(true);
       expect(component.itemLabelControl.value).toEqual(mockItem.title);
+    });
+  });
+
+  describe('onFormSubmit', () => {
+    it('emits the modified event', () => {
+      component.itemLabelControl.setValue('test');
+      spyOn(component.modified, 'next');
+      component.onFormSubmit();
+      expect(component.modified.next).toHaveBeenCalled();
+    });
+
+    it('does not emit modified event for empty values', () => {
+      component.itemLabelControl.setValue('');
+      spyOn(component.modified, 'next');
+      component.onFormSubmit();
+      expect(component.modified.next).not.toHaveBeenCalled();
+    });
+
+    it('creates item copy', () => {
+      component.item = { ...mockItem };
+      spyOn(component.modified, 'next');
+      component.onFormSubmit();
+      expect(component.modified.next).not.toHaveBeenCalledWith(component.item);
+    });
+  });
+
+  describe('onPriorityToggleClick', () => {
+    it('creates item copy', () => {
+      component.item = { ...mockItem };
+      spyOn(component.modified, 'next');
+      component.onPriorityToggleClick();
+      expect(component.modified.next).not.toHaveBeenCalledWith(component.item);
+    });
+
+    it('appropriately toggles item priority', () => {
+      component.item = { ...mockItem };
+      spyOn(component.modified, 'next');
+      component.onPriorityToggleClick();
+
+      expect(component.modified.next).toHaveBeenCalledWith({
+        ...mockItem,
+        priority: 'high',
+      });
+    });
+  });
+
+  describe('onMarkDoneToggleClick', () => {
+    it('creates item copy', () => {
+      component.item = { ...mockItem };
+      spyOn(component.modified, 'next');
+      component.onMarkDoneToggleClick();
+      expect(component.modified.next).not.toHaveBeenCalledWith(component.item);
+    });
+
+    it('appropriately toggles the isDone flag', () => {
+      component.item = { ...mockItem };
+      spyOn(component.modified, 'next');
+      component.onMarkDoneToggleClick();
+
+      expect(component.modified.next).toHaveBeenCalledWith({
+        ...mockItem,
+        isDone: true,
+      });
+    });
+  });
+
+  describe('onDelete', () => {
+    it('emits the delete event', () => {
+      spyOn(component.deleted, 'next');
+      component.onDelete();
+      expect(component.deleted.next).toHaveBeenCalled();
     });
   });
 });
--- src/app/todo/list/list.component.spec.ts
+++ src/app/todo/list/list.component.spec.ts
@@ -1,3 +1,4 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { of } from 'rxjs';
 
@@ -12,6 +13,7 @@ describe('ListComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ListComponent],
+      schemas: [NO_ERRORS_SCHEMA],
       providers: [TodoDataStoreService],
     }).compileComponents();
   });
